7112a2c6aebbc810c9ce4fc092379111f661ad68
merc90 <harshad90@gmail.com>
2023-06-16 12:23:03 +0100

    Email

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -13,7 +13,7 @@ class FeedbackFormTest(TestCase):
         self.assertTrue("Enter a valid email address" in str(email_errors))
 
     def test_correct_email(self):
-        form = forms.FeedbackForm({"email": "test@example.com", "content": "some feedback"})
+        form = forms.FeedbackForm({"email": "test@cabinetoffice.gov.uk", "content": "some feedback"})
         form.is_valid()
         no_email_errors = "email" not in form.errors
         self.assertTrue(no_email_errors)
